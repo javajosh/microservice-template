275a89445c195a560353487a5857e4a01eca3b92
Josh Rehman <javajosh@gmail.com>
2017-06-05 12:06:11 -0700

    First steps in adding an apollo-angular dependency

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,14 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
 import {
   MdButtonModule,
   MdCheckboxModule
 } from '@angular/material';
 
+import { ApolloClient, createNetworkInterface } from 'apollo-client';
+import { ApolloModule } from 'apollo-angular';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GreenSquareComponent } from './green-square/green-square.component';
+
+// by default, this client will send queries to `/graphql` (relative to the URL of your app)
+const client = new ApolloClient({
+  networkInterface: createNetworkInterface({uri: '/graphql'})
+});
+
+export function provideClient(): ApolloClient{
+  return client;
+}
 
 @NgModule({
   declarations: [
@@ -20,6 +33,8 @@ import { GreenSquareComponent } from './green-square/green-square.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+
+    ApolloModule.forRoot(provideClient),
 
     MdButtonModule,
     MdCheckboxModule,
